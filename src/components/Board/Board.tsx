970c9c22c1491f757d3cfaf0f074e790426b3e90
Youssef Elsayed <eng.youssefelsayed03@gmail.com>
2024-10-04 02:30:49 +0300

    win or draw bug solved

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -24,19 +24,19 @@ const Board = () => {
     });
   };
 
+  const isWin = () =>
+    (board[0] === board[1] && board[1] === board[2] && board[0] !== "") ||
+    (board[3] === board[4] && board[4] === board[5] && board[3] !== "") ||
+    (board[6] === board[7] && board[7] === board[8] && board[6] !== "") ||
+    (board[0] === board[3] && board[3] === board[6] && board[0] !== "") ||
+    (board[1] === board[4] && board[4] === board[7] && board[1] !== "") ||
+    (board[2] === board[5] && board[5] === board[8] && board[2] !== "") ||
+    (board[0] === board[4] && board[4] === board[8] && board[0] !== "") ||
+    (board[2] === board[4] && board[4] === board[6] && board[2] !== "");
+
   useEffect(() => {
     if (count < 9) {
-      if (
-        count > 4 &&
-        ((board[0] === board[1] && board[1] === board[2] && board[0] !== "") ||
-          (board[3] === board[4] && board[4] === board[5] && board[3] !== "") ||
-          (board[6] === board[7] && board[7] === board[8] && board[6] !== "") ||
-          (board[0] === board[3] && board[3] === board[6] && board[0] !== "") ||
-          (board[1] === board[4] && board[4] === board[7] && board[1] !== "") ||
-          (board[2] === board[5] && board[5] === board[8] && board[2] !== "") ||
-          (board[0] === board[4] && board[4] === board[8] && board[0] !== "") ||
-          (board[2] === board[4] && board[4] === board[6] && board[2] !== ""))
-      ) {
+      if (isWin()) {
         console.log("Winner");
         setWin(true);
         setCount(10);
@@ -46,8 +46,14 @@ const Board = () => {
         setTurn("O");
       }
     } else if (count === 9) {
-      console.log("Draw");
-      setDraw(true);
+      if (isWin()) {
+        console.log("Winner");
+        setWin(true);
+        setCount(10);
+      } else {
+        console.log("Draw");
+        setDraw(true);
+      }
     }
 
     console.log(board);
